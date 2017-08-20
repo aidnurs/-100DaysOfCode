a89e2cd2e6330cb70c26c71502c65033a3cb16d5
aidnurs <nursultan_imanov@mail.ru>
2017-08-20 21:45:56 +0600

    js can select number

--- projects/jsCalculator/test.js
+++ projects/jsCalculator/test.js
@@ -1,7 +1,23 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
   $('button').click(function() {
     var a=$('button').attr("value");
     console.log(a);
     $("#answer").html();
+  })
+});
+*/
+var current=0;
+
+$(document).ready(function() {
+  $('button').click(function() {
+    var a=($(this).attr('value'));
+    if (isNaN(a)) {
+      
+      console.log(a);
+    }else{
+      current=parseInt(a);
+      $('#answer').html(current);
+      console.log(current);
+    }
   })
 });
