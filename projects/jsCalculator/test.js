e36725c52a06ac04f52de86c98ce6375c390e19c
aidnurs <nursultan_imanov@mail.ru>
2017-09-13 21:58:28 +0200

    calculator

--- projects/jsCalculator/test.js
+++ projects/jsCalculator/test.js
@@ -1,10 +1,30 @@
-new Vue({
-  el: '#app-4',
-  data:{
-  	todos: [
-      { text: 'Learn JavaScript' },
-      { text: 'Learn Vue' },
-      { text: 'Build something awesome' }
-    ]
-  }
-})
+window.onload=function(){
+  var totalNumber=0;
+  var currentNumber=0;
+  $('button').click(function() {
+    if (Number($(this).text())) {
+      currentNumber=Number($(this).text());
+    }else{
+      switch ($(this).text()) {
+        case "C":
+          totalNumber=0;
+          currentNumber=0;
+          break;
+        case "+":
+          totalNumber=totalNumber+currentNumber;
+          break;
+        case "=":
+          $('#current').html(totalNumber);
+          break;
+        default:
+
+          break;
+
+      }
+    }
+    $('#current').html(currentNumber);
+
+
+
+  });
+}
