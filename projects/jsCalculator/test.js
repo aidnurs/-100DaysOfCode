8519aa4def07c622f3bbc967a65fb21841dee223
aidnurs <nursultan_imanov@mail.ru>
2017-09-13 20:15:37 +0200

    vuejs

--- projects/jsCalculator/test.js
+++ projects/jsCalculator/test.js
@@ -1,20 +1,10 @@
-window.onload=function() {
-  var current=0;
-  var total=0;
-
-  var arr1=document.querySelectorAll('button');
-  console.log(arr1);
-  arr1.forEach(function(a) {
-    a.addEventListener('click', function(elem) {
-      current=parseInt(a.id);
-      console.log(current);
-      console.log(typeof(current));      document.getElementById('currentResult').innerHTML=current;
-      });
-      document.getElementById('result').innerHTML=total;
-
-  });
-  /*a.forEach(a=>a.addEventListener('click', function() {
-    console.log(1);
-  }););*/
-
-}
+new Vue({
+  el: '#app-4',
+  data:{
+  	todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+})
