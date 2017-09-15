442e56a93d31b370f3e1e1565c4d81ec3c92012c
aidnurs <nursultan_imanov@mail.ru>
2017-09-15 20:43:17 +0200

    calc engine

--- projects/jsCalculator/test.js
+++ projects/jsCalculator/test.js
@@ -1,18 +1,73 @@
 window.onload=function(){
   var input="";
-  var currentNumber=0;
-  var arr=[];
-  $('.numbers').click(function() {
+  var firstNumber=0;
+  var secondNumber=0;
+  var operator="";
+  var ans=0;
+  var text="";
+  var nextNumber=false;
+  $('.numbers').click(function(){
+    if (ans!==0) {
+      input="";
+    }
     input+=$(this).text();
-    console.log(input);
+    $('#current').html(input);
+  });
+  $('.operators').click(function(){
+    operator=$(this).text();
+    firstNumber=Number(input);
+    input="";
   });
   $('#equal').click(function() {
-
-  });
-  $(".operators").click(function() {
-    currentNumber=Number(input);
-    arr.push(currentNumber);
+    secondNumber=Number(input);
     input="";
-    console.log(arr);
-  })
+    switch (operator) {
+      case '/':
+        ans=firstNumber/secondNumber;
+        break;
+      case '*':
+        ans=firstNumber*secondNumber;
+        break;
+      case '+':
+        ans=firstNumber+secondNumber;
+        break;
+      case '-':
+        ans=firstNumber-secondNumber;
+        break;
+
+      default:break;
+    }
+    input=ans.toString();
+    $('#current').html(ans);
+  });
+  $('#C').click(function() {
+    input="";
+    firstNumber=0;
+    secondNumber=0;
+    ans=0;
+    operator="";
+    $('#current').html(ans);
+  });
 };
+
+/*
+switch (input) {
+  case 'C':
+    firstNumber=0;
+    secondNumber=0;
+    ans=0;
+    break;
+  case '/':break;
+  case '*':break;
+  case '+':
+    ans=firstNumber+secondNumber;
+    secondNumber=0;
+    firstNumber=ans;
+    $('#current').html(ans);
+    break;
+  case '-':break;
+  case '=':
+
+    break;
+  default:
+*/
